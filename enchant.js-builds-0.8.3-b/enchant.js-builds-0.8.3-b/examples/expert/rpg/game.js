a5c87676d26e6d39aa706dac1f65c645759bafef
simaenaga <tamie.taniguchi@gmail.com>
2019-03-10 11:22:35 +0900

    ループ音声停止、鏡飛込みと調整

--- enchant.js-builds-0.8.3-b/enchant.js-builds-0.8.3-b/examples/expert/rpg/game.js
+++ enchant.js-builds-0.8.3-b/enchant.js-builds-0.8.3-b/examples/expert/rpg/game.js
@@ -337,9 +337,9 @@ window.onload = function() {
         image.draw(game.assets['chara0.gif'], 32*6, 0, 96, 128, 0, 0, 96, 128);
         player.image = image;
 
-        State=GameEvent;
+        /*State=GameEvent;
         eventKind=1;
-        talkProgress=-1;
+        talkProgress=-1;*/
 
         //プレイヤーの動き作成(いじらない)
         player.isMoving = false;
@@ -809,7 +809,7 @@ window.onload = function() {
         var buttonSound = game.assets['musmus_btn_set\\btn01.mp3'].clone();
 
 
-
+        //ループ音声再生
         var M_Sound=new SoundLoop(game);
         M_Sound.Set(obakeSound);
 
@@ -1039,9 +1039,12 @@ window.onload = function() {
         image.draw(game.assets['chara0.gif'], 32*6, 0, 96, 128, 0, 0, 96, 128);
         player.image = image;
 
-        State=GameEvent;
+        /*State=GameEvent;
         eventKind=1;
-        talkProgress=-1;
+        talkProgress=-1;*/
+        State=Nomal;
+        eventKind=0;
+        talkProgress=0;
 
         //プレイヤーの動き作成(いじらない)
         player.isMoving = false;
@@ -1135,6 +1138,8 @@ window.onload = function() {
             }else if(eventKind==1 && talkProgress == 5){
                 player.tick++;
                 if(player.tick == 68){
+                    buttonSound.play();
+
                     message=makeMessage("ミラ「かがみの向こう、なのかな」");
                     scene.addChild(message);
                     talkProgress++;
@@ -1173,10 +1178,35 @@ window.onload = function() {
             if(eventKind==1 && talkProgress == 11){
                 player.tick++;
                 if(player.tick == 130){
+                  buttonSound.play();
+
                     message=makeMessage("ミラ「......とりあえず進んでみよう。帰るのはいつでもできるよね」");
                     scene.addChild(message);
                     talkProgress++;
                 }
+            }else if(eventKind==2 && talkProgress == 4){
+                obakeEnterSound.play();
+                message=screenDark(0.6);
+                scene.addChild(message);
+                player.tick=0;
+                talkProgress++;
+            }else if(eventKind==2 && talkProgress == 5){
+                player.tick++;
+                if(player.tick==16){
+                    message=screenDark(0.6);
+                    scene.addChild(message);
+                    player.tick=0;
+                    talkProgress++;
+                }
+            }else if(eventKind==2 && talkProgress == 6){
+                player.tick++;
+                if(player.tick==16){
+                    message=screenDark(1);
+                    scene.addChild(message);
+                    player.tick=0;
+                    talkProgress++;
+                }
+            }else if(eventKind==2 && talkProgress == 7){
 
             }
 
@@ -1225,7 +1255,6 @@ window.onload = function() {
                                         break;
 
                                     case 1:
-                                        buttonSound.play();
                                         scene.removeChild(message);
                                         talkProgress++;
                                         break;
@@ -1242,7 +1271,7 @@ window.onload = function() {
                                         break;
 
                                     case 6:
-                                        buttonSound.play();
+
                                         scene.removeChild(message);
                                         talkProgress++;
                                     break;
@@ -1254,7 +1283,6 @@ window.onload = function() {
                                         break;
 
                                     case 9:
-                                        buttonSound.play();
                                         scene.removeChild(message);
                                         talkProgress++;
                                         break;
@@ -1267,7 +1295,6 @@ window.onload = function() {
                                         break;
 
                                     default:
-                                        buttonSound.play();
                                         scene.removeChild(message);
                                         eventKind=0;
                                         talkProgress=0;
@@ -1293,13 +1320,23 @@ window.onload = function() {
                                         talkProgress++;
                                         break;
                                     case 3:
-                                        buttonSound.play();
+                                        M_Sound.Stop(Sound);//ループＢＧＭの停止
+
                                         scene.removeChild(message);
-                                      talkProgress++;
-                                      break;
-                                  default:
-                                      obakeOut.play();
-                                      talkProgress=0;
+                                        talkProgress++;
+                                        break;
+
+                                    case 4:
+                                        break;
+
+                                    case 5:
+                                        break;
+
+                                    case 6:
+                                        break;
+
+                                    case 7:
+                                        break;
                               }
                                 break;
                             case 3:
